0bcbd260e9678cfe87d5178257afa91a79f303e8
Devin2089 <djsom525@gmail.com>
2025-10-14 15:29:52 +0000

    j

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args)
+	{
+		int x = 4;
+if (x > 0) {
+    if (x % 2 == 0) {
+        System.out.println("Even Positive");
+    }
+}
+else {
+    System.out.println("Non-Positive");
+	}
+}
